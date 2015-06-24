8234bc73a020ef6b4aed39036c2633c7af29a16c
CaptainXD <orcuttkids4him@gmail.com>
2015-06-24 00:42:59 -0700

    Added dynamic table sorting

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
+var id;
+var double = false;
 $(document).ready(function() {
     $.getJSON("./js/table.json", null, function(data) {
         info = data;
         sort();
     });
     twitterClickHandler();
+    $(document).on('click', 'th', function () {
+        if(id === this.id && !double) {
+            double = true;
+        } else {
+            id = this.id;
+            double = false;
+        }
+        sort(this.id);
+    });
 });
--- js/table.js
+++ js/table.js
@@ -1,5 +1,6 @@
 var info;
 var clubs = ["CHI","CLB","COL","DAL","DC","HOU","KC","LA","MTL","NE","NY","NYC","ORL","PDX","PHI","RSL","SEA","SJ","TOR","VAN"];
+var cities = [];
 var gamesPlayed = [];
 var wins = [];
 var draws = [];
@@ -8,41 +9,98 @@ var differential = [];
 var points = [];
 var twitterUsername = [];
 var widgetId = [];
+var th = `<tr>
+            <th id="cities"><a href="javascript:void(0)">Club</a></th>
+            <th id="gamesPlayed"><a href="javascript:void(0)">GP</a></th>
+            <th id="wins"><a href="javascript:void(0)">W</a></th>
+            <th id="draws"><a href="javascript:void(0)">D</a></th>
+            <th id="losses"><a href="javascript:void(0)">L</a></th>
+            <th id="differential"><a href="javascript:void(0)">±</a></th>
+            <th id="points"><a href="javascript:void(0)">P</a></th>
+        </tr>`;
 
-function sort() {
+var choices = [];
+
+function sort(by) {
     for(var i = 0; i < 20; i++) {
         points[clubs[i]] = info[clubs[i]].W * 3 + info[clubs[i]].D;
         differential[clubs[i]] = info[clubs[i]].GD;
+        cities[clubs[i]] = info[clubs[i]].Club;
+        gamesPlayed[clubs[i]] = info[clubs[i]].GP;
+        wins[clubs[i]] = info[clubs[i]].W;
+        draws[clubs[i]] = info[clubs[i]].D;
+        losses[clubs[i]] = info[clubs[i]].GP - info[clubs[i]].W - info[clubs[i]].D;
     }
     var keys = [];
     for (var key in points) {
         keys.push(key);
     }
-
+    
+    choices["points"] = points;
+    choices["differential"] = differential;
+    choices["cities"] = cities;
+    choices["gamesPlayed"] = gamesPlayed;
+    choices["wins"] = wins;
+    choices["draws"] = draws;
+    choices["losses"] = losses;
+    
     keys.sort(function(k0, k1) {
-        var a = points[k0];
-        var b = points[k1];
-        
-        if(a > b) return -1;
-        if(a < b) return 1;
-        if(a === b) {
-            if(differential[k0] > differential[k1]) return -1;
-            if(differential[k1] > differential[k0]) return 1;
-            if(differential[k0] === differential[k1]) {
+        var a;
+        var b;
+        if(typeof by === "undefined") {
+            a = points[k0];
+            b = points[k1];
+        } else {
+            a = choices[by][k0];
+            b = choices[by][k1];
+        }
+        if(!double) {
+            if(by !== "cities") {
+                if(a > b) return -1;
+                if(a < b) return 1;
+                if(a === b && (typeof by === "undefined" || by === "points")) {
+                    if(differential[k0] > differential[k1]) return -1;
+                    if(differential[k1] > differential[k0]) return 1;
+                    if(differential[k0] === differential[k1]) {
+                        if(k0 < k1) return -1;
+                        if(k0 > k1) return 1;
+                        else return 0;
+                    }
+                } else {
+                    if(k0 < k1) return -1;
+                    if(k0 > k1) return 1;
+                }
+            } else {
                 if(k0 < k1) return -1;
                 if(k0 > k1) return 1;
-                else return 0;
+            }
+        } else {
+            if(by !== "cities") {
+                if(a < b) return -1;
+                if(a > b) return 1;
+                if(a === b && (typeof by === "undefined" || by === "points")) {
+                    if(differential[k0] < differential[k1]) return -1;
+                    if(differential[k1] < differential[k0]) return 1;
+                    if(differential[k0] === differential[k1]) {
+                        if(k0 > k1) return -1;
+                        if(k0 < k1) return 1;
+                        else return 0;
+                    }
+                } else {
+                    if(k0 > k1) return -1;
+                    if(k0 < k1) return 1;
+                }
+            } else {
+                if(k0 > k1) return -1;
+                if(k0 < k1) return 1;
             }
         }
     });
-
+    $("table").empty();
+    $("table").append(th);
     for (var i = 0; i < keys.length; ++i) {
         var key = keys[i];
         var value = points[key];
-        gamesPlayed[key] = info[key].GP;
-        wins[key] = info[key].W;
-        draws[key] = info[key].D;
-        losses[key] = info[key].GP - info[key].W - info[key].D;
         twitterUsername[key] = info[key].twitter;
         widgetId[key] = info[key].id;
         $("table").append(
