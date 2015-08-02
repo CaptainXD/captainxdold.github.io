73c7fae5c1d148f92dafe24695a39129f13c4b1b
CaptainXD <orcuttkids4him@gmail.com>
2015-08-01 19:10:55 -0700

    Mobile bug fix attempt #1

--- js/table.js
+++ js/table.js
@@ -96,8 +96,7 @@ function sort(by) {
             }
         }
     });
-    $("table").empty();
-    $("table").append(th);
+    $("table").html(th);
     for (var i = 0; i < keys.length; ++i) {
         var key = keys[i];
         var value = points[key];
