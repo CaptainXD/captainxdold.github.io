6d3f767e7ebd78649fb3e23c31d87705d1db6b80
CaptainXD <orcuttkids4him@gmail.com>
2015-10-03 02:30:44 -0700

    reversion -DELETE-

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
-var id;
-var double = false;
-$(document).ready(function() {
-    $.getJSON("./js/table.json", null, function(data) {
-        info = data;
-        sort();
-    });
-    twitterClickHandler();
-    $(document).on('click', 'th', function () {
-        if(id === this.id && !double) {
-            double = true;
-        } else {
-            id = this.id;
-            double = false;
-        }
-        sort(this.id);
-    });
+ $(document).ready(function() {
+     $.getJSON("./js/table.json", null, function(data) {
+         info = data;
+         sort();
+     });
+     twitterClickHandler();
 });
